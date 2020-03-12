c9f1298757a731c9043d30e52018578294847ec9
okwrtdsh <okwrtdsh@gmail.com>
2020-03-12 16:22:33 +0900

    update .travis.yml

--- deploy_ftp.sh
+++ deploy_ftp.sh
@@ -1,3 +1,9 @@
+#!/bin/bash -eu
+rm -rf _site
+cat _config.yml | sed -e 's/baseurl: "/baseurl: "\/nagahara/' | sed -e 's/https:\/\/nagahara-lab.github.io/http:\/\/ids.osaka-u.ac.jp/' > _config.yml
+bundle exec jekyll build
+rm -rf _site/deploy*
+
 lftp -u $FTP_USER,$FTP_PASS $FTP_SERVER <<END_SCRIPT
 rm -r nagahara
 mirror -R _site nagahara
