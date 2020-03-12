e4bc7f798796bd10974178e7a72694ee78f0d4e5
okwrtdsh <okwrtdsh@gmail.com>
2020-03-12 17:09:03 +0900

    update deploy script

--- deploy_ftp.sh
+++ deploy_ftp.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -eu
-rm -rf _site
-cat _config.yml | sed -e 's/baseurl: "/baseurl: "\/nagahara/' | sed -e 's/https:\/\/nagahara-lab.github.io/http:\/\/ids.osaka-u.ac.jp/' > _config.yml
-bundle exec jekyll build
-rm -rf _site/deploy*
+lftp -c "set ftp:use-mdtm off;set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASS $FTP_SERVER; mirror -Re -x .git -x .ssh --verbose=3 --ignore-time _site $FTP_DIR --parallel=20" &
+pid=$! # Process Id of the previous running command
 
-lftp -u $FTP_USER,$FTP_PASS $FTP_SERVER <<END_SCRIPT
-rm -r nagahara
-mirror -R _site nagahara
-END_SCRIPT
+spin='-\|/'
+
+i=0
+while kill -0 $pid 2>/dev/null
+do
+  i=$(( (i+1) %4 ))
+  printf "\r${spin:$i:1}"
+  sleep .1
+done
