32b7f993a7e181a73243d700c1b4a769704ee7ea
okwrtdsh <okwrtdsh@gmail.com>
2020-03-14 01:03:26 +0900

    update links

--- deploy_ftp.sh
+++ deploy_ftp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
-lftp -c "set ftp:use-mdtm off;set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASS $FTP_SERVER; mirror -Re -x .git -x .ssh --verbose=3 --ignore-time _site $FTP_DIR --parallel=20" &
+lftp -c "set ftp:use-mdtm off;set ftp:ssl-allow no; open -u $FTP_USER,$FTP_PASS $FTP_SERVER; mirror -Re -x .git -x .ssh --verbose=3 --ignore-time _site $FTP_DIR --parallel=2" &
 pid=$! # Process Id of the previous running command
 
 spin='-\|/'
