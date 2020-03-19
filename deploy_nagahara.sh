503aee8a2328dfb055dd1c071d1d58f775dab572
okwrtdsh <okwrtdsh@gmail.com>
2020-03-19 15:14:05 +0900

    update

--- deploy_nagahara.sh
+++ deploy_nagahara.sh
@@ -4,4 +4,4 @@ rm -rf deploy* secrets.tar*
 git init
 git add .
 git commit -m "Publishing site on `date "+%Y-%m-%d %H:%M:%S"`"
-git push -f git@github.com.io:nagahara-lab/nagahara-lab.github.io.git master:master
+git push -f git@github.com.jp:nagahara-lab/ids.osaka-u.ac.jp-nagahara.git master:master
